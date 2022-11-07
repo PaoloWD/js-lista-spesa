d847b505beea3fa994df9980f3539b964c7d60b3
PaoloWD <paolo.lore.93@gmail.com>
2022-11-07 21:00:38 +0100

    some test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-const listaSpesaEl = document.querySelector(".text-center");
+const listaSpesaEl = document.querySelector(".list-container");
 const listaSpesa = [];
-while (listaSpesa.length < 5) {
+let listUl = document.createElement("ul");
+let listLi = document.createElement("li");
+listLi.classList.add("text-center");
+listLi.classList.add("bg-danger");
+while (listaSpesa.length < 3) {
   const input = prompt("Inserisci articoli");
   listaSpesa.push(input);
+
+  listaSpesaEl.append(listUl);
+  listUl.append(listLi);
 }
-listaSpesaEl.innerHTML = listaSpesa;
+listLi.innerHTML += `${listaSpesa}`;
+
 console.log(listaSpesa);
